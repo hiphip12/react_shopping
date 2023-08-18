7496be8d84a7fe559ab42cc1d8b999ffaf01d9f5
pablo oliva <pablo.oliva@hotmail.fi>
2023-08-18 12:08:47 +0300

    uploading quick setup

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import Button from 'react-boostrap/Button';
+
+// import {Button} from 'react-bootstrap';
+
+const Test = () => {
+
+
+    return <div>
+        <Button variant="secondary">Example Button </Button>
+    </div>
+}
+
+export default Test;
